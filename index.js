c70114e3f914d4d00ce17bb0f2cffd2c3c48c3ed
Niranjan <niranjants0500@gmail.com>
2026-02-27 16:29:25 +0530

    Update response message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Deployment Successful 🚀");
+  res.send("CI/CD update Successful 🚀");
 });
 
 app.listen(PORT, () => {
